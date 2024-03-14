20ad68c53df48f172cbac6a10431ed886c9d0832
elsiip <elsi,puspita1@gmail.com>
2024-03-14 22:45:17 +0700

    feat: perbaikan

--- src/pages/order/orderHeader.jsx
+++ src/pages/order/orderHeader.jsx
@@ -1,4 +1,4 @@
-import Button from "../../components/button";
+import Button from "../../components/button"
 import { formatNumber } from "../../utils/helper";
 
 export default function OrderHeader({
