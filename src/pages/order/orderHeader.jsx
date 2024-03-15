fdc2c31fc0a24e1ce9ba436a5d13eee510c596c8
elsiip <elsi,puspita1@gmail.com>
2024-03-15 07:10:05 +0700

    feat: :zap: add path for button order burger

--- src/pages/order/orderHeader.jsx
+++ src/pages/order/orderHeader.jsx
@@ -1,4 +1,6 @@
-import Button from "../../components/button"
+import React from "react";
+import { useHistory } from "react-router-dom"; // Import useHistory from react-router-dom
+import Button from "../../components/button";
 import { formatNumber } from "../../utils/helper";
 
 export default function OrderHeader({
@@ -6,14 +8,23 @@ export default function OrderHeader({
   selectedIngredients = [],
   onReset,
 }) {
-  
+  const history = useHistory(); // Initialize useHistory
+
   const isEmptySelectedIngredients = selectedIngredients.length === 0;
 
   const totalCost = () => {
     const filterBySelectedIngredients = selectedIngredients.map(
       (id) => allIngredients.find((item) => item?.id === id)?.price
-    );    
-    return `Rp${formatNumber(filterBySelectedIngredients.reduce((a,b) => a + b))}`;
+    );
+    return `Rp${formatNumber(
+      filterBySelectedIngredients.reduce((a, b) => a + b, 0)
+    )}`;
+  };
+
+  // Function to handle order completion
+  const handleOrderCompletion = () => {
+    // Redirect to /order=done
+    history.push("/order=done");
   };
 
   return (
@@ -25,7 +36,12 @@ export default function OrderHeader({
           </div>
         )}
         <div className="flex items-center gap-4 max-w-sm w-full">
-          <Button disabled={isEmptySelectedIngredients}>Order Burger</Button>
+          <Button
+            disabled={isEmptySelectedIngredients}
+            onClick={handleOrderCompletion} // Add onClick event to call handleOrderCompletion
+          >
+            Order Burger
+          </Button>
           {!isEmptySelectedIngredients && (
             <Button variant="secondary" onClick={onReset}>
               Reset
